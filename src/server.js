ce579c996f1831f2843e1a3428513719184abbb6
Jorge Guimarães <jorgeguimaraes@cinbesa.com.br>
2019-10-15 14:00:41 -0300

    add socketio

--- src/server.js
+++ src/server.js
@@ -3,8 +3,17 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const cors = require("cors");
 const path = require("path");
+const socktio = require("socket.io");
+const http = require('http');
 
 const app = express();
+const server = http.Server(app);
+const io = socktio(server);
+
+
+io.on('connection', socket => {
+  console.log('usuario conectado', socket.id);
+})
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@omnistack9-qyvu6.mongodb.net/semana09?retryWrites=true&w=majority",
@@ -16,4 +25,4 @@ app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
